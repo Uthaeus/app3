5f08cd2bdb069f5e0ac37ff2b1ff03582b62c303
Uthaeus <Uth@github.com>
2017-12-27 21:46:18 -0700

    Created pages controller and product scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  resources :products
+  get 'pages/home'
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  get 'pages/faq'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
